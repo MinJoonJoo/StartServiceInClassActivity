95ae25e28b84ce5fd37a69820cc6e72678589d31
MinJoon Joo <tuk34896@temple.edu>
2024-04-04 12:11:09 -0400

    following the steps for the startcommand and coundownstartbutton

--- app/src/main/java/edu/temple/startserviceinclassactivity/CountdownService.kt
+++ app/src/main/java/edu/temple/startserviceinclassactivity/CountdownService.kt
@@ -3,13 +3,31 @@ package edu.temple.startserviceinclassactivity
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import android.util.Log
 
+const val COUNTDOWN_START_VALUE = "countdownValue"
 class CountdownService : Service() {
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        intent?.run{
+            val from = getIntExtra(COUNTDOWN_START_VALUE,10)
+            CoroutineScope(Dispatchers.IO).launch{
+                repeat(from){
+                    Log.d("Countdown",(10-it).toString())
+                    delay(1000)
+                }
+            }
+        }
+
+
         return super.onStartCommand(intent, flags, startId)
     }
 }
--- app/src/main/java/edu/temple/startserviceinclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/startserviceinclassactivity/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.startserviceinclassactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,10 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val countdownValue = findViewById<EditText>(R.id.countdownEditText)
+        val intent = Intent(this, CountdownService::class.java)
+            .putExtra(COUNTDOWN_START_VALUE,countdownValue.text.toString())
 
         val countdownStartButton = findViewById<Button>(R.id.countdownButton)
-
+        countdownStartButton.setOnClickListener {
+            startService(intent)
+        }
     }
 }
